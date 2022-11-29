ff0c9d7d75e6de90d15d4161be54696a1d4b4f0d
LDavid7 <david.lukacs@search-lab.hu>
2022-11-29 16:50:48 +0100

    mongodb ssl enabled

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -10,7 +10,7 @@ auth="-u user -p $MONGODB_PASSWORD"
 (
 echo "setup mongodb auth"
 create_user="if (!db.getUser('user')) { db.createUser({ user: 'user', pwd: '$MONGODB_PASSWORD', roles: [ {role:'readWrite', db:'piggymetrics'} ]}) }"
-until mongo piggymetrics --eval "$create_user" || mongo piggymetrics $auth --eval "$create_user"; do sleep 5; done
+until mongo piggymetrics --eval "$create_user" --ssl --sslAllowInvalidHostnames --sslAllowInvalidCertificates --sslPEMKeyFile=/tmp/server_mongodb_cert_key.pem --sslPEMKeyPassword=piggymetrics --sslCAFile=/tmp/ca_certificate_mongodb.pem || mongo piggymetrics $auth --eval "$create_user" --ssl --sslAllowInvalidHostnames --sslAllowInvalidCertificates --sslPEMKeyFile=/tmp/server_mongodb_cert_key.pem --sslPEMKeyPassword=piggymetrics --sslCAFile=/tmp/ca_certificate_mongodb.pem; do sleep 5; done
 killall mongod
 sleep 1
 killall -9 mongod
@@ -20,14 +20,14 @@ killall -9 mongod
 (
 if test -n "$INIT_DUMP"; then
     echo "execute dump file"
-	until mongo piggymetrics $auth $INIT_DUMP; do sleep 5; done
+	until mongo piggymetrics $auth $INIT_DUMP --ssl --sslAllowInvalidHostnames --sslAllowInvalidCertificates --sslPEMKeyFile=/tmp/server_mongodb_cert_key.pem --sslPEMKeyPassword=piggymetrics --sslCAFile=/tmp/ca_certificate_mongodb.pem; do sleep 5; done
 fi
 ) &
 
 echo "start mongodb without auth"
 chown -R mongodb /data/db
-gosu mongodb mongod "$@"
+gosu mongodb mongod "$@" --config /etc/mongod.conf --bind_ip_all
 
 echo "restarting with auth on"
 sleep 5
-exec gosu mongodb /usr/local/bin/docker-entrypoint.sh --auth "$@"
+exec gosu mongodb /usr/local/bin/docker-entrypoint.sh --auth "$@" --config /etc/mongod.conf --bind_ip_all
